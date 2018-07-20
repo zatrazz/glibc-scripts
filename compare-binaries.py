544a5d855b80ecae45b048bc88a92f21b837291e
Adhemerval Zanella <adhemerval.zanella@linaro.org>
2018-07-20 15:27:10 +0000

    compare-binaries: Add symbol support

--- compare-binaries.py
+++ compare-binaries.py
@@ -5,6 +5,7 @@ import argparse
 import subprocess
 import tempfile
 import configparser
+import re
 from pathlib import Path
 
 PATHS = {}
@@ -25,22 +26,27 @@ def read_config():
 
 def elf_architecture(filename):
   fields = subprocess.check_output(['file', filename]).decode("utf-8")
-  # /bin/ls: ELF 64-bit LSB executable, ARM aarch64, version 1 (SYSV),
-  # dynamically linked, interpreter /lib/ld-linux-aarch64.so.1,
-  # for GNU/Linux 3.7.0, BuildID[sha1]=..., stripped
   fields = fields.split(':')[1].split(',')
   if not fields[0].strip().startswith('ELF'):
     return None
   return fields[1].strip()
 
-def objdump_tool(arch):
+def tool_path(arch, tool):
   architectures = {
     "ARM aarch64" : [ "aarch64-linux-gnu", "aarch64-glibc-linux-gnu" ]
   };
   if not arch in architectures:
     return None
   return PATHS['compilers'] + '/' + architectures[arch][0] + '/bin/' \
-         + architectures[arch][1] + '-objdump'
+         + architectures[arch][1] + '-' + tool
+
+def symbol_in_list(symbol, symbols):
+  r = re.compile(r'.*\b' + symbol + r'\b')
+  for s in list(filter(None.__ne__, [r.search(s) for s in symbols ])):
+    fields = s.group(0).split()
+    start = int(fields[0], 16)
+    size  = int(fields[1], 16)
+    return [ start, start + size ]
 
 def run_objdump_diff(file1, file2, symbol):
   arch1 = elf_architecture (file1)
@@ -49,19 +55,44 @@ def run_objdump_diff(file1, file2, symbol):
     print("error: ELF files have different architectures (%s, %s)" %
           arch1, arch2)
     return
+  if not arch1 or not arch2:
+    print("error: invalid input object file")
+    return
 
-  objdump = objdump_tool(arch1)
+  objdump = tool_path(arch1, 'objdump')
+  objdump_args1 = []
+  objdump_args2 = []
+
+  if symbol:
+    nm = tool_path(arch1, 'nm')
+
+    out1 = subprocess.check_output([nm, "-S", '--size-sort', file1])
+    out1 = out1.decode('utf-8').splitlines()
+    range1 = symbol_in_list (symbol, out1)
+
+    out2 = subprocess.check_output([nm, "-S", '--size-sort', file2])
+    out2 = out2.decode('utf-8').splitlines()
+    range2 = symbol_in_list (symbol, out2)
+
+    objdump_args1 += [ '--section=.text',
+                       '--start-address=0x%x' % range1[0],
+                       '--stop-address=0x%x' % range1[1] ]
+    objdump_args2 += [ '--section=.text',
+                       '--start-address=0x%x' % range2[0],
+                       '--stop-address=0x%x' % range2[1] ]
+
+
   mode = None
   try:
     tmpb = tempfile.NamedTemporaryFile(delete=False)
-    outb = subprocess.check_output([objdump, "-d", file1])
+    outb = subprocess.check_output([objdump, "-d", file1] + objdump_args1)
     tmpb.write(outb)
 
     tmpp = tempfile.NamedTemporaryFile(delete=False)
-    outp = subprocess.check_output([objdump, "-d", file2])
+    outp = subprocess.check_output([objdump, "-d", file2] + objdump_args2)
     tmpp.write(outp)
 
-    diffp = subprocess.Popen(["diff", "-u", tmpb.name, tmpp.name],
+    diffp = subprocess.Popen(["diff", "-y", tmpb.name, tmpp.name],
                              stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     diff, err = diffp.communicate()
     sys.stdout.buffer.write(diff)
