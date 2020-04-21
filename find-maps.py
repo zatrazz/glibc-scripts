951819bc67c954a299a057450c9802691bfa0d14
Adhemerval Zanella <adhemerval.zanella@linaro.org>
2020-04-21 11:42:11 -0300

    find-maps: Rewrite
    Use argparser.

--- find-maps.py
+++ find-maps.py
@@ -1,17 +1,42 @@
 #! /usr/bin/env python
 
 import sys
+import argparse
+
+"""
+find-maps.py is a script to find the memory map of the given address from
+a specified pid.
+"""
+
+def find_map(pid, address):
+  with open ("/proc/" + str (pid) + "/maps", "r") as f:
+    for line in f:
+      line = line.replace('\n', '')
+      fields = line.split()
+      ran = fields[0].split('-')
+      start = int (ran[0], 16)
+      end = int (ran[1], 16)
+      if address >= start and address < end:
+        print (" => " + line)
+      else:
+        print ("    " + line)
+
+def get_parser ():
+  def auto_int(x):
+     return int(x, 0)
+  parser = argparse.ArgumentParser(description=__doc__)
+  parser.add_argument('pid',
+                      help='The process id',
+                      type=int)
+  parser.add_argument('address',
+                      help='The memory address',
+                      type=auto_int)
+  return parser
+
+def main (argv):
+  parser = get_parser ()
+  opts = parser.parse_args (argv)
+  find_map (opts.pid, opts.address)
 
 if __name__ == "__main__":
-  pid = sys.argv[1]
-  addr = int(sys.argv[2], 16)
-
-  maps = open ("/proc/" + pid + "/maps")
-  for line in maps:
-    fields = line.split()
-    ran = fields[0].split('-')
-    start = int (ran[0], 16)
-    end = int (ran[1], 16)
-    if addr >= start and addr < end:
-      print (line)
-      break
+  main(sys.argv[1:])
